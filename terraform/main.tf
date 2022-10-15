fae1190b6c163adcf8f3486af658c24f0e1799d7
Victor Batista <victorbatistax@gmail.com>
2022-10-15 14:20:02 -0300

    criando duas novas contas

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "aft.poc.01@gmail.com"
+    AccountName               = "sandbox-aft-public"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "aft.poc.01@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
@@ -24,4 +24,32 @@ module "sandbox" {
   }
 
   account_customizations_name = "sandbox"
+}
+
+module "prod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aft.poc.02@gmail.com"
+    AccountName               = "prod-aft-private"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "aft.poc.02@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "production"
 }
